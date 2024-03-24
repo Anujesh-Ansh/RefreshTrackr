acc444f21da8802cedb5b0950177af068c829a40
Anujesh Ansh <anshanujesh@gmail.com>
2024-03-24 13:17:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [refreshCount, setRefreshCount] = useState(0);
+
+  useEffect(() => {
+    // Reset refresh count to 0 when component mounts or updates
+    const count = parseInt(localStorage.getItem('refreshCount')) || 0;
+    setRefreshCount(count);
+
+    const handleBeforeUnload = () => {
+      localStorage.setItem('refreshCount', count + 1);
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [refreshCount]); // Add refreshCount as dependency to ensure useEffect runs on every refresh
+
+  const handleResetOnClick = () => {
+    localStorage.setItem('refreshCount', '0');
+    setRefreshCount(0);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={handleResetOnClick} style={{ cursor: 'pointer' }}>
+          <code>
+            Refresh Count: {refreshCount}
+          </code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Anujesh-Ansh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub Profile
         </a>
       </header>
     </div>
